d9c5be4b9b790a97dcc96a9c99060161c153bc7f
Stv-devl <lansadstevan@gmail.com>
2025-03-19 11:53:53 +0700

    push sample

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './assets/styles/global.css';
+
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error('can not find root in the dom');
+}
+const root = ReactDOM.createRoot(rootElement as HTMLElement);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
