651bc85c668faaae611355c0280aa4f70a9f4abf
Stv-devl <lansadstevan@gmail.com>
2025-03-19 15:00:42 +0700

    add font, be ready component, time to have fun

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className="h-screen">
-      <header className="App-header"></header>
+    <div className=" h-screen dark:text-white bg-zinc-100 text-neutral-900 dark:bg-gray-800">
+      <header>Hello world</header>
     </div>
   );
 }
---
+++ src/componants/CoutryWrapper/CountryWrapper.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CountryWrapper = () => {
+  return <div></div>;
+};
+
+export default CountryWrapper;
